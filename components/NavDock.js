3bfb3e352485ab0ec76caf7d32530a073cf341e6
Brian Travis <bztravis88@gmail.com>
2023-07-04 00:27:24 -0400

    Add basic <NavDock>

--- components/NavDock.js
+++ components/NavDock.js
@@ -1,16 +1,38 @@
+'use client'
+
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import styles from '@/styles/components/NavDock.module.scss'
+import New from '@/public/icons/New.svg'
+import Widget from '@/public/icons/Widget.svg'
 
 export default function NavDock() {
+  const router = useRouter()
+
   return (
     <div className={styles.container}>
+      <div className={styles.menu}>
+        <Widget
+          onClick={() => {
+            router.push('/')
+          }}
+        />
+        <New
+          onClick={() => {
+            router.push('/new')
+          }}
+        />
+      </div>
       <Image
-        className={styles.pfp}
+        className={styles.profile}
         src="/profile.png"
         width={36}
         height={36}
         alt="pfp"
         priority
+        onClick={() => {
+          router.push('/account')
+        }}
       ></Image>
     </div>
   )
