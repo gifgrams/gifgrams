257d72200175611db000f58f2b0eb87db7b854e9
Brian Travis <bztravis88@gmail.com>
2023-08-07 21:34:42 -0400

    Add client side route protection to /new

--- app/(nestedPages)/new/page.js
+++ app/(nestedPages)/new/page.js
@@ -1,17 +1,32 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
+'use client'
+
+import { useEffect } from 'react'
+// import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+// import { cookies } from 'next/headers'
+import { useRouter } from 'next/navigation'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import NavBar from '@/components/NavBar'
 import styles from '@/styles/app/newPage.module.scss'
 
-export const metadata = {
-  title: 'New Card – GifGrams',
-}
+export default function App() {
+  // const supabase = createServerComponentClient({ cookies })
+  // const {
+  //   data: { session },
+  // } = await supabase.auth.getSession()
 
-export default async function App() {
-  const supabase = createServerComponentClient({ cookies })
-  const {
-    data: { session },
-  } = await supabase.auth.getSession()
+  const supabase = createClientComponentClient()
+  const router = useRouter()
+
+  useEffect(() => {
+    const protectRoute = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession()
+      console.log('session client /auth', session)
+      if (!session) router.push('/signup')
+    }
+    protectRoute()
+  }, [])
 
   return (
     <div className={styles.container}>
