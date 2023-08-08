40aa6696aedfee45c28ad4a20725aba0f2575c73
Brian Travis <bztravis88@gmail.com>
2023-08-08 10:41:01 -0500

    Update <TextInput> to support containerStyles

--- ui/TextInput.js
+++ ui/TextInput.js
@@ -3,13 +3,20 @@
 import { useId } from 'react'
 import styles from '@/styles/ui/TextInput.module.scss'
 
-export default function TextInput({ label, placeholder, ...inputProps }) {
+export default function TextInput({
+  label,
+  placeholder,
+  containerStyle,
+  ...inputProps
+}) {
   const id = useId()
   return (
-    <div className={styles.container}>
-      <label htmlFor={id}>
-        <h3>{label ?? ''}</h3>
-      </label>
+    <div className={styles.container} style={containerStyle}>
+      {label !== undefined && (
+        <label htmlFor={id}>
+          <h3>{label}</h3>
+        </label>
+      )}
       <input id={id} placeholder={placeholder ?? ''} {...inputProps}></input>
     </div>
   )
