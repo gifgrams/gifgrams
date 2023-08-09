011f11f78bba4beccd5e8e8ce08b12692fcd392e
Brian Travis <bztravis88@gmail.com>
2023-08-09 15:33:54 -0800

    Add <GifGramsTag>

---
+++ components/GifGramsTag.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '@/styles/components/GifGramsTag.module.scss'
+
+export default function GigGramsTag() {
+  return (
+    <div className={styles.container}>
+      <Link href="/">
+        <Image
+          src="/wordmark.svg"
+          alt="wordmark"
+          width={208}
+          height={42}
+          priority
+        ></Image>
+      </Link>
+    </div>
+  )
+}
