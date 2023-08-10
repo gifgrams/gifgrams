d59f7bed911f0edfa4e94fa3e25b37ecdc3a0d3f
Brian Travis <bztravis88@gmail.com>
2023-08-10 11:15:17 -0800

    Update <Card> responsiveness for addition of <CardControls> of

--- components/CardContainer.js
+++ components/CardContainer.js
@@ -1,11 +1,28 @@
 import Card from '@/components/Card'
+import CardControls from '@/components/CardControls'
 import GifGramsTag from '@/components/GifGramsTag'
 import styles from '@/styles/components/CardContainer.module.scss'
 
-export default function CardContainer({ isPreview = false, cardData }) {
+export default function CardContainer({
+  isPreview = false,
+  cardData = {
+    mediaUrl: '',
+    accentColor: '#E0E0E0',
+    typeface: 'Monserrat',
+    fontSize: 14,
+    fontColor: '#000000',
+    backgroundColor: '#FFFFFF',
+    message: '',
+    title: '',
+    recipientName: '',
+    recipientEmail: '',
+    sendDate: Date.now(),
+  },
+}) {
   return (
     <div className={styles.container}>
       <Card cardData={cardData} />
+      <CardControls />
       {!isPreview && <GifGramsTag />}
     </div>
   )
