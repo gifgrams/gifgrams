1a590b96472125b945767a1770175eb089511dbe
Brian Travis <bztravis88@gmail.com>
2023-08-10 23:24:53 -0800

    Add reactions panel

--- components/CardControls.js
+++ components/CardControls.js
@@ -1,6 +1,8 @@
 import UndoLeftRoundSquare from '@/public/icons/UndoLeftRoundSquare.svg'
 import styles from '@/styles/components/CardControls.module.scss'
 
+const reactions = ['😁', '😂', '❤️', '🙏', '🥳']
+
 export default function CardControls() {
   return (
     <div className={styles.container}>
@@ -8,6 +10,11 @@ export default function CardControls() {
         <UndoLeftRoundSquare />
         Send a free response
       </button>
+      <div className={styles.reactionContainer}>
+        {reactions.map((elem) => (
+          <button>{elem}</button>
+        ))}
+      </div>
     </div>
   )
 }
