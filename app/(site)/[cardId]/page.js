938f0a3663062b4354d4c1951711d359684b1962
Brian Travis <bztravis88@gmail.com>
2023-08-22 13:35:10 -0400

    Add /[cardId] dynamic metadata title

--- app/(site)/[cardId]/page.js
+++ app/(site)/[cardId]/page.js
@@ -2,6 +2,21 @@ import { cookies } from 'next/headers'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import CardIdContainer from '@/components/CardIdContainer'
 
+export async function generateMetadata({ params }) {
+  // read route params
+  const id = params.cardId
+
+  // fetch data
+  const supabase = createServerComponentClient({ cookies })
+  const { data, error } = await supabase.from('card').select().eq('id', id)
+  console.log('error', error)
+  console.log('data', data)
+
+  return {
+    title: data?.[0].card_data.title,
+  }
+}
+
 export default async function Card({ params }) {
   const id = params.cardId
 
