306c218336a50c4c23116d15b69e75e80665d42a
Brian Travis <bztravis88@gmail.com>
2023-08-22 11:19:36 -0400

    Add send history fetch and display

--- app/page.js
+++ app/page.js
@@ -13,8 +13,14 @@ export const metadata = {
 export default async function App() {
   const supabase = createServerComponentClient({ cookies })
   const {
-    data: { session },
-  } = await supabase.auth.getSession()
+    data: { user },
+  } = await supabase.auth.getUser()
+  const { data, error } = await supabase
+    .from('card')
+    .select()
+    .eq('user_id', user.id)
+    .order('created_at', { ascending: false })
+  console.log('data', data)
 
   return (
     <div className={styles.container}>
@@ -26,13 +32,11 @@ export default async function App() {
           </Link>
           <h1>Send History</h1>
           <div className={styles.cardContainer}>
-            {Array(5)
-              .fill(null)
-              .map((elem, index) => (
-                <Link key={index} href="/asdf" target="_blank">
-                  <HistoryCard />
-                </Link>
-              ))}
+            {data?.map((elem, index) => (
+              <Link key={index} href={`/${elem.id}`} target="_blank">
+                <HistoryCard card={elem} />
+              </Link>
+            ))}
           </div>
         </div>
       </main>
