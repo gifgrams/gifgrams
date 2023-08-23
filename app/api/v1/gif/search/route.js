7f18b15d1a69dc25682deddb0aa8d7a80cddb49b
Brian Travis <bztravis88@gmail.com>
2023-08-22 21:11:04 -0400

    Add error tolerance to tenor search api routes

--- app/api/v1/gif/search/route.js
+++ app/api/v1/gif/search/route.js
@@ -4,16 +4,22 @@ export async function GET(req) {
   const { searchParams } = new URL(req.url)
   const query = searchParams.get('query')
 
-  const res = await fetch(
-    `https://tenor.googleapis.com/v2/search?q=${query}&key=${process.env.TENOR_API_KEY}&client_key=my_test_app&limit=48`,
-    {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
-  )
-  const data = await res.json()
-  // const filteredData = data.results.map(())
-
-  return NextResponse.json({ data })
+  try {
+    const res = await fetch(
+      `https://tenor.googleapis.com/v2/search?q=${query}&key=${process.env.TENOR_API_KEY}&client_key=my_test_app&limit=48`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+    const data = await res.json()
+    return NextResponse.json({ data })
+  } catch (error) {
+    console.error('error', error)
+    return NextResponse.json(
+      { message: 'Error fetching from Tenor' },
+      { status: 500 }
+    )
+  }
 }
