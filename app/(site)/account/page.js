472ab2b40e6a22f767dad8937e089661118ba1f5
Brian Travis <bztravis88@gmail.com>
2023-08-23 13:16:30 -0400

    Revert to /account server component

--- app/(site)/account/page.js
+++ app/(site)/account/page.js
@@ -1,51 +1,26 @@
-'use client'
-
-import { useState, useEffect } from 'react'
-// import { cookies } from 'next/headers'
-// import { redirect } from 'next/navigation'
-import { useRouter } from 'next/navigation'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 import Image from 'next/image'
-// import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import NavBar from '@/components/NavBar'
 import SignOut from '@/components/SignOut'
 import styles from '@/styles/app/accountPage.module.scss'
 
-// export const dynamic = 'force-dynamic'
-// export const fetchCache = 'force-no-store'
-// export const dynamicParams = true
-// export const revalidate = 0
+export const revalidate = 0
 
-// export const metadata = {
-//   title: 'Account – GifGrams',
-// }
+export const metadata = {
+  title: 'Account – GifGrams',
+}
 
-export default function Account() {
-  const supabase = createClientComponentClient()
-  const router = useRouter()
-  const [sessionState, setSession] = useState()
-  // const supabase = createServerComponentClient({ cookies })
+export default async function Account() {
+  const supabase = createServerComponentClient({ cookies })
 
-  // const {
-  //   data: { session },
-  //   error,
-  // } = await supabase.auth.getSession()
-  // if (error) console.log('Error in getSession() in NavDock.js', error)
-  // if (!session) redirect('/signup')
-
-  useEffect(() => {
-    const protectRoute = async () => {
-      const {
-        data: { session },
-      } = await supabase.auth.getSession()
-      // console.log('session client /account', session)
-      if (!session) router.push('/signup')
-      setSession(session)
-      // console.log('session?.user.email', session?.user.email)
-    }
-    protectRoute()
-  }, [])
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.getSession()
+  if (error) console.log('Error in getSession() in NavDock.js', error)
+  if (!session) redirect('/signup')
 
   return (
     <>
@@ -59,7 +34,7 @@ export default function Account() {
           height={304}
           priority
         ></Image>
-        <h1>{sessionState !== undefined && sessionState?.user.email}</h1>
+        <h1>{session.user.email}</h1>
         <SignOut />
       </main>
     </>
