cdc9ca9f77f040c498560eeeb0d2cbf8de87f4e7
Brian Travis <bztravis88@gmail.com>
2023-08-25 10:13:55 -0400

    Fix attempt for Chillax font on windows

--- components/NewProgress.js
+++ components/NewProgress.js
@@ -1,10 +1,19 @@
 import styleBuilder from '@/util/styleBuilder'
 import RoundArrowLeft from '@/public/icons/RoundArrowLeft.svg'
 import styles from '@/styles/components/NewProgress.module.scss'
+import localFont from 'next/font/local'
+
+const chillax = localFont({
+  src: [
+    {
+      path: '../fonts/Chillax-Variable.ttf',
+    },
+  ],
+})
 
 export default function NewProgress({ stage, setStage }) {
   return (
-    <div className={styles.container}>
+    <div className={styleBuilder([styles.container, chillax.className])}>
       {stage > 0 && (
         <button
           className={styles.back}
