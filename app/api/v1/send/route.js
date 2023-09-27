0d41e174451740c07496c2da9ae7ca7917cba98b
Brian Travis <bztravis88@gmail.com>
2023-09-27 00:51:57 -0400

    Add basic sendgrid email post

--- app/api/v1/send/route.js
+++ app/api/v1/send/route.js
@@ -10,5 +10,26 @@ export async function POST(req) {
   const { error } = await supabase.from('card').insert(body)
   // console.log('error', error)
 
+  const sgMail = require('@sendgrid/mail')
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+  const msg = {
+    to: 'bztravis@umich.edu', // Change to your recipient
+    from: 'noreply@gifgrams.com', // Change to your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  }
+
+  sgMail
+    .send(msg)
+    .then((response) => {
+      console.log(response[0].statusCode)
+      console.log(response[0].headers)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+
   return NextResponse.json({ error })
 }
