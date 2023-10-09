1000bfd7b4979c3d63e938b8940dad06ff607dc4
Brian Travis <bztravis88@gmail.com>
2023-10-09 02:04:11 -0400

    Add styling to card onboarding

--- app/api/v1/send/route.js
+++ app/api/v1/send/route.js
@@ -28,8 +28,8 @@ export async function POST(req) {
     to: body.card_data.recipientEmail, // Change to your recipient
     from: 'noreply@gifgrams.com', // Change to your verified sender
     subject: `${profile.full_name} sent you a greeting: ${body.card_data.title}`,
-    text: `${profile.full_name} sent you a GifGram! Open it here: https://gifgrams.com/${body.card_data.id}`,
-    html: `<p>${profile.full_name} sent you a GifGram! </p><strong>${body.card_data.title}</strong><p>Open it here: https://gifgrams.com/${body.id}</p>`,
+    text: `${profile.full_name} sent you a GifGram! ${body.card_data.title} Open your virtual greeting here: https://gifgrams.com/${body.id}`,
+    html: `<p>${profile.full_name} sent you a GifGram! </p><strong>${body.card_data.title}</strong><p>Open your virtual greeting here: https://gifgrams.com/${body.id}</p>`,
   }
 
   console.log('msg', msg)
