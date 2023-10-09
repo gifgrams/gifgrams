ac2d372e7a7badaafc19a22495aff97545d7fd51
Brian Travis <bztravis88@gmail.com>
2023-10-08 22:30:02 -0400

    Add response feature coming soon alerts

--- components/CardControls.js
+++ components/CardControls.js
@@ -5,17 +5,26 @@ import styles from '@/styles/components/CardControls.module.scss'
 const reactions = ['😁', '😂', '❤️', '🙏', '🥳']
 
 export default function CardControls({ isFront }) {
+  const alertComingSoon = (feature) => {
+    alert(`${feature} are coming soon! Hang tight :)`)
+  }
+
   return (
     <div
       className={styleBuilder([styles.container, [styles.isFront, isFront]])}
     >
-      <button>
+      <button onClick={() => alertComingSoon('Email respones')}>
         <UndoLeftRoundSquare />
         Send a free response
       </button>
       <div className={styles.reactionContainer}>
         {reactions.map((elem, index) => (
-          <button key={index}>{elem}</button>
+          <button
+            key={index}
+            onClick={() => alertComingSoon('Emoji reactions')}
+          >
+            {elem}
+          </button>
         ))}
       </div>
     </div>
