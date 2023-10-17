4eaa997223e9741e1e4fa8968b18b9fff54fac59
Brian Travis <bztravis88@gmail.com>
2023-10-16 21:05:52 -0400

    Update respond post route boilerplates

--- app/api/v1/respond/message/route.js
+++ app/api/v1/respond/message/route.js
@@ -4,5 +4,24 @@ import sgMail from '@sendgrid/mail'
 export async function POST(req) {
   const body = await req.json()
 
-  return NextResponse.json()
+  const msg = {
+    to: body.card_data.recipientEmail, // Change to your recipient
+    from: 'noreply@gifgrams.com', // Change to your verified sender
+    subject: `${profile.full_name} sent you a greeting: ${body.card_data.title}`,
+    text: `${profile.full_name} sent you a GifGram! ${body.card_data.title} Open your virtual greeting here: https://gifgrams.com/${body.id}`,
+    html: `<p>${profile.full_name} sent you a GifGram! </p><strong>${body.card_data.title}</strong><p>Open your virtual greeting here: https://gifgrams.com/${body.id}</p>`,
+  }
+  console.log('msg', msg)
+
+  const { response: sgResponse, error: sgError } = await sgMail.send(msg)
+  console.log(sgResponse?.[0].statusCode)
+  console.log(sgResponse?.[0].headers)
+  console.error(sgError)
+
+  return NextResponse.json(
+    sgError ? { error: sgError } : { message: 'Success' },
+    {
+      status: sgError ? 500 : 200,
+    }
+  )
 }
