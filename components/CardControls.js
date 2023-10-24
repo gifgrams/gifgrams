0af6fcdf771a8ad425dcbda0b4618a0dded89694
Brian Travis <bztravis88@gmail.com>
2023-10-23 23:57:32 -0400

    Add some emoji reaction function code

--- components/CardControls.js
+++ components/CardControls.js
@@ -1,4 +1,7 @@
+'use client'
+
 import styleBuilder from '@/util/styleBuilder'
+import emitToast from '@/ui/Toast'
 import UndoLeftRoundSquare from '@/public/icons/UndoLeftRoundSquare.svg'
 import styles from '@/styles/components/CardControls.module.scss'
 
@@ -7,6 +10,22 @@ const reactions = ['😁', '😂', '❤️', '🙏', '🥳']
 export default function CardControls({ isFront }) {
   const alertComingSoon = (feature) => {
     alert(`${feature} are coming soon! Hang tight :)`)
+  }
+
+  const sendEmojiReaction = (emoji) => {
+    fetch('/api/v1/respond/emoji', {
+      method: 'POST',
+    })
+      .then((response) => {
+        console.log('response', response)
+        return response.json()
+      })
+      .then((data) => {
+        console.log('data', data)
+      })
+      .catch((error) => {
+        console.log('error', error)
+      })
   }
 
   return (
