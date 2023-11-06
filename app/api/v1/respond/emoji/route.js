bd515a8b0de25a91c42709b1c1256f8d33060bb4
Brian Travis <bztravis88@gmail.com>
2023-11-06 00:57:34 -0500

    Add emoji reaction email

--- app/api/v1/respond/emoji/route.js
+++ app/api/v1/respond/emoji/route.js
@@ -1,13 +1,15 @@
+import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import sgMail from '@sendgrid/mail'
+import formatEmojiEmail from './formatEmojiEmail'
 
 export async function POST(req) {
   const body = await req.json()
 
   console.log('body', body)
 
-  const supabase = createRouteHandlerClient()
+  const supabase = createRouteHandlerClient({ cookies })
   let { data: card, error } = await supabase
     .from('card')
     .select('*')
@@ -16,18 +18,29 @@ export async function POST(req) {
   console.log('cardfirst')
   console.log('card', card)
 
+  console.log('ENV', process.env.SENDGRID_API_KEY)
+
   const msg = {
-    to: card.card_data.user_email, // Change to your recipient
+    to: card[0].user_email, // Change to your recipient
     from: 'noreply@gifgrams.com', // Change to your verified sender
-    subject: `${card.card_data.recipientName} reacted to your GifGram: ${body.emoji}`,
-    text: `${profile.full_name} sent you a GifGram! ${body.card_data.title} Open your virtual greeting here: https://gifgrams.com/${body.id}`,
-    html: `<p>${profile.full_name} sent you a GifGram! </p><strong>${body.card_data.title}</strong><p>Open your virtual greeting here: https://gifgrams.com/${body.id}</p>`,
+    subject: `${card[0].card_data.recipientName} reacted to your GifGram: ${body.emoji}`,
+    text: `${card[0].card_data.recipientName} reacted to your GifGram: ${body.emoji}`,
+    html: formatEmojiEmail(
+      card[0].card_data.recipientName,
+      `https://gifgrams.com/${card[0].id}`,
+      body.emoji
+    ),
   }
   console.log('msg', msg)
 
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
   const { response: sgResponse, error: sgError } = await sgMail.send(msg)
+  console.log('bruh')
+  console.error(JSON.stringify(sgError))
+
   console.log(sgResponse?.[0].statusCode)
   console.log(sgResponse?.[0].headers)
+  console.log('bruh', sgResponse?.body?.errors)
   console.error(sgError)
 
   return NextResponse.json(
